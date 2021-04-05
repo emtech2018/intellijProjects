66339fbee1e9ecf580d57894447e3c8a3cfa70bd
emtech2018 <info@emtechhouse.co.ke>
2021-04-05 22:25:50 +0300

    iiii

---
+++ src/com/emtech/Main.java
@@ -0,0 +1,13 @@
+package com.emtech;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("project 1");
+        System.out.println("now again");
+        System.out.println("now agai7");
+        System.out.println("tresttstts");
+        System.out.println("tretts");
+    }
+}
